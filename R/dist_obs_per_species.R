a4e6e8ce26a38a4e82b35651ac9b4f4666cd7876
Coralie Williams <coralie.williams@unsw.edu.au>
2024-12-04 18:07:49 +1100

    case study and folder set-up

---
+++ R/dist_obs_per_species.R
@@ -0,0 +1,18 @@
+# k.species - number of species
+k.species <- c(10^8)
+
+# simulate number of observations per species
+k.obs <- round(rbeta(n = k.species, shape1 = 1.25, shape2 = 3) * 99) + 1
+k.obs
+hist(k.obs)
+min(k.obs)
+max(k.obs)
+mean(k.obs)
+as.numeric(names(sort(-table(k.obs)))[1])
+median(k.obs)
+
+
+##### covariate x
+x <- runif(10^6, 10, 20) 
+hist(x)
+median(x)
